32f8f0529d4764c174dd95667c118f456bd4344b
Jeovani Kotounou <borisjeovani@gmail.com>
2026-06-03 02:41:48 +0200

    Correction STATICFILES_STORAGE

--- refaccom/settings.py
+++ refaccom/settings.py
@@ -160,3 +160,5 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
