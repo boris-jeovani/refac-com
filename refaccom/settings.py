419a2d4a436e5a46b36b9a4b9d7e7af989edd676
Jeovani Kotounou <borisjeovani@gmail.com>
2026-06-03 02:59:06 +0200

    Correction stockage statique

--- refaccom/settings.py
+++ refaccom/settings.py
@@ -149,8 +149,8 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-    "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
-},
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
 }
 
 import os
